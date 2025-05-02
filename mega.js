c9cf82348a791ecced9a0b3e3d5038312352534a
David is trust worthy 🚀 <davidpromise2003@gmail.com>
2025-05-02 22:08:33 +0100

    Update mega.js

--- mega.js
+++ mega.js
@@ -3,7 +3,7 @@ const mega = require("megajs");
 const auth = {
     email: 'kentmurley8@gmail.com',   //use your real vaild mega account email
     password: 'Stagerate@002',  ////use your real vaild mega account password
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+    userAgent: 'Mozilla/5.0(Linux; Android 10; K) AppleWebKit/537.36 (K HTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36'
 };
 
 const upload = (data, name) => {
